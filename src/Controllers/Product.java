6eda34afb4201d164c662e7d1be962c80bbc2863
Curran Buss <curranbuss@gmail.com>
2022-12-01 21:37:35 -0600

    Fixed Product class

--- src/Controllers/Product.java
+++ src/Controllers/Product.java
@@ -1,16 +1,12 @@
 package Controllers;
 
-import javafx.beans.property.*;
-
 public class Product {
+
+	// Class variables
 	private String item;
 	private int quantity;
 	private double amount;
 	private String thumbImage;
-
-	private StringProperty item_name;
-	private DoubleProperty item_amount;
-	private DoubleProperty item_qty;
 
 	public Product(String item, double amount, int quantity, String thumbImage) {
 		super();
@@ -23,82 +19,49 @@ public class Product {
 			System.out.println("Quantity must be greater than 0");
 		} else {
 			this.quantity = quantity;
-			this.amount = amount * quantity;
 		}
 	}
 
-	public Product() {
-		this.item_name = new SimpleStringProperty();
-		this.item_amount = new SimpleDoubleProperty();
-		this.item_qty = new SimpleDoubleProperty();
+	public Product(String item, double amount, int quantity) {
+		super();
+		this.item = item;
+		this.quantity = quantity;
+		this.amount = amount;
+
+		if (quantity <= 0) {
+			System.out.println("Quantity must be greater than 0");
+		} else {
+			this.quantity = quantity;
+		}
 	}
 
-	public String getItem() {
-		return item;
-	}
-
+	// Product variable setters
 	public void setItem(String item) {
 		this.item = item;
 	}
-
-	public int getQuantity() {
-		return quantity;
-	}
-
 	public void setQuantity(int quantity) {
 		if (quantity <= 0) {
 			System.out.println("Quantity must be greater than 0");
 		} else {
 			this.quantity = quantity;
-			this.amount = amount * quantity;
+		}
+	}
+	public void setAmount(double amount) {
+		if (amount < 0) {
+			System.out.println("Price must be greater than or equal to zero!");
+		} else {
+			this.amount = amount;
 		}
 	}
 
-	public double getAmount() {
-		return amount;
-	}
-
-	public void setAmount(double amount) {
-		this.amount = amount;
-	}
-
-	public StringProperty prodNameProperty() {
-		return this.item_name;
-	}
-
-	public void setThumbImage(String thumbImage) {
-		this.thumbImage = thumbImage;
-	}
-
-	public String getThumbImage() {
-		return thumbImage;
-	}
-
-	public void setProdName(String name) {
-		this.item_name.set(name);
-	}
-
-	public DoubleProperty prodAmountProperty() {
-		return this.item_amount;
-	}
-
-	public void setProdQty(Double qty) {
-		this.item_qty.set(qty);
-	}
-
-	public DoubleProperty prodQtyProperty() {
-		return this.item_qty;
-	}
-
-	public void setProdAmount(Double amount) {
-		this.item_amount.set(amount);
-	}
-	
+	// Product variable getters
+	public String getItem() { return item; }
+	public int getQuantity() { return quantity; }
+	public double getAmount() { return amount; }
+	public String getThumbImage() { return thumbImage; }
 
 	@Override
 	public String toString() {
-		return String.format("%-35s%-20d%10.2f", getItem(), getQuantity(), getAmount());
+		return String.format("%s\t\t%d\t\t%.02f", getItem(), getQuantity(), getAmount());
 	}
-	
-	
 }
